b77f482aba8f1ee8440843c99ec7b521dfcd70a9
Karol-Jakubik <61844514+Karol-Jakubik@users.noreply.github.com>
2020-06-11 23:50:35 +0200

    Update project_initial.py

--- project_initial.py
+++ project_initial.py
@@ -14,12 +14,13 @@ import requests
 def transcription():
     words = input('Type or paste your sentence(s) below:\n')
     words_list = words.split()
-    print("getting your transcription ready, please wait")
+    print("Getting your transcription ready, please wait")
     
     for word in words_list:
         page = requests.get('https://dictionary.cambridge.org/dictionary/english/' + str(word))
         tree = html.fromstring(page.content)
         Words = tree.xpath('//span[@class="ipa dipa lpr-2 lpl-1"]/text()')
-        word1 = Words[0]
+        word1 = Words[1]
         print(word1)
+        
 transcription()
