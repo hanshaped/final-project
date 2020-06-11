b4a83b3fbd25ba0ef8ecddedf1c42a872ac50e74
Karol-Jakubik <61844514+Karol-Jakubik@users.noreply.github.com>
2020-06-11 19:11:17 +0200

    Update project_initial.py
    adding old code that could work for our project

--- project_initial.py
+++ project_initial.py
@@ -12,5 +12,42 @@ from multiprocessing import Pool
 def transcription():
     sentences = input('Type or paste your sentence(s) below:\n')
     print(sentences)
+
+#def main():
+    #print('Getting staff urls ...')
+
+    #staff_url='http://wa.amu.edu.pl/wa/en/staff_list'
+   # staff_content = get_content(staff_url)
     
+   # links = staff_content.find_all('a')    
+
+   # urls = []
+
+#    for link in links:
+  #      if len(link.get_text()) > 1:
+   #         base_url = 'http://wa.amu.edu.pl'
+   #         url= urllib.parse.urljoin(base_url, link['href'])
+    #        encoded_url = fix_encoding(url)
+     #       urls.append(encoded_url)
+    
+#    print('Staff emails found:')    
+    
+
+#    mypool = Pool()
+ #   mypool.map(print_details, urls)
+
+
+
+#def fix_encoding(url):
+#     components = urllib.parse.urlsplit(url)
+ #    components = list(components)
+ #    components[2] = urllib.parse.quote(components[2])
+ #    return urllib.parse.urlunsplit(components)
+ 
+#def get_content(url):
+ #   response = urllib.request.urlopen(url)
+ #   data= response.read()
+  #  doc = BeautifulSoup(data, 'html.parser')
+  #  return doc.find(id = 'tresc_wlasciwa')    
+
 transcription()
