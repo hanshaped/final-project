784f042e980454ff244db1749790be60e330482b
Karol-Jakubik <61844514+Karol-Jakubik@users.noreply.github.com>
2020-06-11 23:45:12 +0200

    Update project_initial.py
    change of plans

--- project_initial.py
+++ project_initial.py
@@ -4,47 +4,22 @@ Created on Mon Jun  1 18:32:37 2020
 
 @author: Hanna Szczepanek
 """
-import urllib.request
-from bs4 import BeautifulSoup
-import urllib.parse
-from multiprocessing import Pool
-from tkinter import Tk
 
-#def ipa(sentences):
-#    print('Getting your transcription ...')
 
-#    dic_url='https://dictionary.cambridge.org/dictionary/english/'
-#    dic_content = get_content(dic_url)
-    
-#    phonetic = dic_content.find_all('a')    
+from lxml  import html
+import requests
 
-#    phonemes = []
 
-#    for word in words:
-#        if len(word.get_text()) > 1:
-#            base_url = 'https://dictionary.cambridge.org'
-#            url = urllib.parse.urljoin(base_url, link['href'])
-#            encoded_url = fix_encoding(url)
-#            urls.append(encoded_url)
-    
-#    print('Your transcription: ')    
-    
+
 def transcription():
     words = input('Type or paste your sentence(s) below:\n')
     words_list = words.split()
-    print(words)
-#    ipa(words)
-
-#def fix_encoding(url):
- #   components = urllib.parse.urlsplit(url)
- #   components = list(components)
- #   components[2] = urllib.parse.quote(components[2])
- #   return urllib.parse.urlunsplit(components)
- 
-#def get_content(url):
-#    response = urllib.request.urlopen(url)
-#    data= response.read()
-#    doc = BeautifulSoup(data, 'html.parser')
-#    return doc.find(id = 'tresc_wlasciwa')    
-
+    print("getting your transcription ready, please wait")
+    
+    for word in words_list:
+        page = requests.get('https://dictionary.cambridge.org/dictionary/english/' + str(word))
+        tree = html.fromstring(page.content)
+        Words = tree.xpath('//span[@class="ipa dipa lpr-2 lpl-1"]/text()')
+        word1 = Words[0]
+        print(word1)
 transcription()
