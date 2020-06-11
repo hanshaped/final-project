5f68e5172f3f66d1d786ba4958284f2e6f42e853
Hanna Szczepanek <61844552+hanshaped@users.noreply.github.com>
2020-06-11 22:27:40 +0200

    Updating the interface

--- view.py
+++ view.py
@@ -5,20 +5,28 @@ Created on Thu Jun 11 21:19:56 2020
 @author: Hanna Szczepanek
 """
 
-from tkinter import Tk, Label, 
+import tkinter as tk
 
 class View:
     def __init__(self, root, controller):
         self.root = root
         self.controller = controller
         
-        self.info_label = tk.Label(root, text='Welcome to TransSCRIBE!')
-        self.info_label.pack()
+        self.info_label1 = tk.Label(root,
+                                    text='Welcome to TransSCRIBE!',
+                                    fg='#00244d')
+        self.info_label1.pack()
+        
+        self.info_label2 = tk.Label(root, text='TransSCRIBE allows for the basic transciption of a list of words or sentences into the phonetic alphabet.')
+        self.info_label2.pack()
+        
+        self.info_label3 = tk.Label(root, text='In short, you can see how to pronounce them!')
+        self.info_label3.pack()
         
         self.entry = tk.Entry(root)
         self.entry.pack()
         
-        self.button = tk.Button(root, text="", command=self.get_word)
+        self.button = tk.Button(root, text="TransSCRIBE!", command=self.get_word)
         self.button.pack()
         
         self.transcription_label = tk.Label(root)
@@ -26,11 +34,11 @@ class View:
         
     def get_word(self):
         word = self.entry.get()
-        print(word)
+        self.controller.word_entered(word)
         
     def display_transcription(self, transcription):
         self.transcription_label.configure(text="Transcribtion:/n" + transcription)
         
 root = tk.Tk()
-view = view(root, None)
+view = View(root, None)
 root.mainloop()
