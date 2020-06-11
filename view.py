d428833b6a50c1776b4ba59401b7d6304b44bcef
Hanna Szczepanek <61844552+hanshaped@users.noreply.github.com>
2020-06-11 23:07:08 +0200

    Working on the interface

--- view.py
+++ view.py
@@ -6,22 +6,48 @@ Created on Thu Jun 11 21:19:56 2020
 """
 
 import tkinter as tk
+from tkinter import BOTH, TOP, RIGHT, N, LEFT, CENTER
 
 class View:
     def __init__(self, root, controller):
         self.root = root
+        self.root.title("TransSCRIBE – Check How To Pronounce!")
         self.controller = controller
+        
+        self.frame = tk.Frame(root, bg='#cceeff')
+        self.frame.pack()
         
         self.info_label1 = tk.Label(root,
                                     text='Welcome to TransSCRIBE!',
-                                    fg='#00244d')
-        self.info_label1.pack()
+                                    padx=10,
+                                    pady=10,
+                                    fg='#cceeff',
+                                    bg='#004466',
+                                    font=('times',22,'bold','italic'))
+        self.info_label1.pack(expand=True, fill=BOTH, side=TOP)
         
-        self.info_label2 = tk.Label(root, text='TransSCRIBE allows for the basic transciption of a list of words or sentences into the phonetic alphabet.')
-        self.info_label2.pack()
+        self.info_label2 = tk.Label(root,
+                                    text='TransSCRIBE allows for the basic transciption of a list of words or sentences into the phonetic alphabet.',
+                                    padx=15,
+                                    pady=15,
+                                    justify=CENTER,
+                                    wraplength=500,
+                                    fg='#004466',
+                                    bg='#cceeff',
+                                    font=('times',12))
+        self.info_label2.pack(expand=True, fill=BOTH, side=TOP)
         
-        self.info_label3 = tk.Label(root, text='In short, you can see how to pronounce them!')
-        self.info_label3.pack()
+        self.info_label3 = tk.Label(root,
+                                    text='In short, you can see how to pronounce them!',
+                                    padx=15,
+                                    pady=15,
+                                    anchor=N,
+                                    justify=CENTER,
+                                    wraplength=500,
+                                    fg='#004466',
+                                    bg='#cceeff',
+                                    font=('times',12,'bold'))
+        self.info_label3.pack(expand=True, fill=BOTH, side=TOP)
         
         self.entry = tk.Entry(root)
         self.entry.pack()
@@ -38,7 +64,3 @@ class View:
         
     def display_transcription(self, transcription):
         self.transcription_label.configure(text="Transcribtion:/n" + transcription)
-        
-root = tk.Tk()
-view = View(root, None)
-root.mainloop()
