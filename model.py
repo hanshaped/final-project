f276abec40d9a24d38ec130c9f45aeafb909a83e
Hanna Szczepanek <61844552+hanshaped@users.noreply.github.com>
2020-06-12 13:00:16 +0200

    Some regex experimentation

--- model.py
+++ model.py
@@ -7,6 +7,7 @@ Created on Thu Jun 11 21:42:49 2020
 
 from lxml  import html
 import requests
+import re
 
 class Model:
     def __init__(self, controller):
@@ -18,16 +19,32 @@ class Model:
         
         
     def get_transcription(self, words):
-        # words = input('Type or paste your sentence(s) below:\n')
         words_list = words.split()
         print("Getting your transcription ready, please wait")
         transcribed_words = ""
-    
+        
         for word in words_list:
             page = requests.get('https://dictionary.cambridge.org/dictionary/english/' + str(word))
             tree = html.fromstring(page.content)
             Words = tree.xpath('//span[@class="ipa dipa lpr-2 lpl-1"]/text()')
             word1 = Words[1]
-            transcribed_words = transcribed_words + " " + word1
+            #pattern1 = '\b.+(aæɑəeɛɜɪɔɒouʊʌiː)s\b'
+            #Word_class = tree.xpath('//span[@class="pos dpos"]/text()')
+            #if Word_class[1] == 'noun':
+                #plural = ''
+                #isitplural = 0
+                #result1 = re.match(pattern1, word1)
+                #if result1 == 'None':
+                    #return isitplural
+                #else:
+                    #isitplural = 1
+                    #if isitplural == 1:
+                        #plural == 'ɪz'
+                    #else:
+                        #plural == ''
+                #return plural
+            #else:
+                #pass
+            transcribed_words = transcribed_words + " " + word1 #+ plural
         
         return(transcribed_words)
